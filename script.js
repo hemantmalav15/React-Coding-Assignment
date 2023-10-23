733da33116c93746e733aeb05697d64f8656d9c5
Hemant Malav <96330614+hemantmalav15@users.noreply.github.com>
2023-10-23 20:29:42 +0530

    JSX and Components in React

--- script.js
+++ script.js
@@ -1,88 +1,41 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
-// this is how we can create and nest Element using React
-{
-    /* lets say we want to create this 
+import ReactDOM from "react-dom/client";
+
+// JSX (React Element) = HTML like syntax -> React.createElement -> Object -> DOM
+// React Element using JSX
+const heading = <h1>Namaste React</h1>;
+// React Element
+const container = (
     <div>
+        <h1>Namaste React</h1>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
         </ul>
-    </div> */
-}
-// let li = React.createElement("li", {}, "This is list item");
-let ul = React.createElement(
-    "ul",
-    {
-        id:"list"
-    },
-    [
-        React.createElement(
-            "li",
-            {
-
-            },
-            "This is list item1"
-        ),
-        React.createElement(
-            "li",
-            {
-
-            },
-            "This is list item2"
-        ),
-        React.createElement(
-            "li",
-            {
-
-            },
-            "This is list item3"
-        ),
-        React.createElement(
-            "li",
-            {
-
-            },
-            "This is list item4"
-        )
-    ]
+    </div>
 );
 
-let div = React.createElement("div", {}, [ul]);
-let heading = React.createElement(
-    "h1",
-    {
-        id: "heading1",
-        className: "h1"
-    },
-    "This is heading1",
-    React.createElement(
-        "p",
-        {
-            id: "para"
-        },
-        "This is paragraph inside h1",
-    )
-);
-let heading2 = React.createElement(
-    "h2",
-    {
-        id: "heading2",
-        className: "h2"
-    },
-    "This is heading2"
-);
+/**
+ * Components -> Everything in React is Component, and there are two types of components in React
+ * 1. Functional Component - NEW 
+ * 2. Class Based Component - OLD
+ * 
+ * functional components is a normal javascript function.
+ */
 
-let div1 = React.createElement(
-    "div",
-    {
-        id: "root-div"
-    },
-    [heading, heading2]
+// function Header() {
+//     return (
+//         <h1>Namaste React</h1>
+//     );
+// }
+
+// Both are same thing -> we can skip return and curly braces
+const Header = () => (
+    <h1>Namaste React</h1>
 );
 
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div);
+root.render(<Header />);
