7e48efcd5726c8fa91adc344ba8753e559cc118b
Hemant Malav <96330614+hemantmalav15@users.noreply.github.com>
2023-10-25 03:17:56 +0530

    Implemented search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constants";
+import { useState } from "react";
 
-const Body = () => (
-    <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
-            return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-        })}
-    </div>
-)
+const Body = () => {
+    function filterData(searchText, restaurants) {
+        return restaurants.filter((restaurant) => (
+            restaurant?.data?.name?.toLowerCase().includes(searchText.toLowerCase())
+        ))
+    }
+    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [searchText, setSearchText] = useState("");
+    return (
+        <>
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Search"
+                value={searchText}
+                onChange={(e) => {
+                    setSearchText(e.target.value);
+                }} />
+            <button className="search-btn" onClick={() => {
+                const data = filterData(searchText, restaurant);
+                // console.log(data)
+                setRestaurants(data);
+            }}
+            >
+                Search
+            </button>
+
+            <div className="restaurant-list">
+                {restaurants.map((restaurant) => {
+                    return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+                })}
+            </div>
+        </>
+    )
+}
+
 
 export default Body;
