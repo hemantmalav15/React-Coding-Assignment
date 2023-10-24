2e05cc009b7b47862db81ede880e1eec7b568491
Hemant Malav <96330614+hemantmalav15@users.noreply.github.com>
2023-10-25 02:19:27 +0530

    Assignment-5 done

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => (
+    <div className="header">
+        <h1 className="logo">Khaosa</h1>
+        <ul className="nav-items">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+    </div>
+)
+export default Header;
