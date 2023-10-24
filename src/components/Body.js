8b5152ea638396043cc39585948f9f2ba30749b3
Hemant Malav <96330614+hemantmalav15@users.noreply.github.com>
2023-10-25 05:28:01 +0530

    Made search functionality working

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,6 +8,7 @@ const Body = () => {
             restaurant?.data?.name?.toLowerCase().includes(searchText.toLowerCase())
         ))
     }
+    const [filteredRestaurant, setFilteredRestaurant] = useState(restaurantList);
     const [restaurants, setRestaurants] = useState(restaurantList);
     const [searchText, setSearchText] = useState("");
     return (
@@ -21,16 +22,16 @@ const Body = () => {
                     setSearchText(e.target.value);
                 }} />
             <button className="search-btn" onClick={() => {
-                const data = filterData(searchText, restaurant);
+                const data = filterData(searchText, restaurants);
                 // console.log(data)
-                setRestaurants(data);
+                setFilteredRestaurant(data);
             }}
             >
                 Search
             </button>
 
             <div className="restaurant-list">
-                {restaurants.map((restaurant) => {
+                {filteredRestaurant.map((restaurant) => {
                     return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
                 })}
             </div>
